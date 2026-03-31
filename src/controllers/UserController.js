a6e02e72b77078515d9230a44cf56f9aa89ebcb2
jose <joseamando5974@gmail.com>
2026-03-31 17:43:26 -0300

    receiving and validating users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,15 +3,11 @@ import User from '../models/User';
 class UserController {
   async store(req, res) {
     try {
-      const novoUser = await User.create({
-        nome: 'João',
-        email: 'joao.silva@example.com',
-        password: '123456',
-      });
+      const novoUser = await User.create(req.body);
       return res.json(novoUser);
     } catch (e) {
       return res.status(400).json({
-        error: e.message ? e.error.map((err) => err.message) : [e.message],
+        errors: e.errors.map((err) => err.message),
       });
     }
   }
--- src/models/User.js
+++ src/models/User.js
@@ -4,48 +4,57 @@ import bcrypt from 'bcryptjs';
 
 export default class User extends Model {
   static init(sequelize) {
-    super.init({
-      nome: {
-        type: Sequelize.STRING,
-        defaultValue: '',
-        validate: {
-          len: {
-            args: [3, 255],
-            msg: 'O nome deve conter entre 3 e 255 caracteres.',
+    super.init(
+      {
+        nome: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [3, 255],
+              msg: 'O nome deve conter entre 3 e 255 caracteres.',
+            },
+          },
+        },
+        email: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          unique: {
+            msg: 'Email já cadastrado.',
+          },
+          validate: {
+            notEmpty: {
+              msg: 'Email não pode ser vazio',
+            },
+            isEmail: {
+              msg: 'Email inválido',
+            },
+          },
+        },
+        password_hash: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+        },
+        password: {
+          type: Sequelize.VIRTUAL,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [6, 50],
+              msg: 'A senha deve conter entre 6 e 50 caracteres.',
+            },
           },
         },
       },
-      email: {
-        type: Sequelize.STRING,
-        validate: {
-          notEmpty: {
-            msg: 'Email não pode ser vazio',
-          },
-          isEmail: {
-            msg: 'Email inválido',
-          },
-        },
+      {
+        sequelize,
       },
-      password_hash: {
-        type: Sequelize.STRING,
-        defaultValue: '',
-      },
-      password: {
-        type: Sequelize.VIRTUAL,
-        defaultValue: '',
-        validate: {
-          len: {
-            args: [6, 50],
-            msg: 'A senha deve conter entre 6 e 50 caracteres.',
-          },
-        },
-      },
-    }, {
-      sequelize,
-    });
+    );
 
     this.addHook('beforeSave', async (user) => {
-      user.password_hash = await bcrypt.hash(user.password, 8);
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
     });
     return this;
   }
