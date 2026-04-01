1cd986f5899ec6607a3575f59313336bf41e38b5
jose <joseamando5974@gmail.com>
2026-04-01 17:16:06 -0300

    add jws token and login validations

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -11,6 +11,64 @@ class UserController {
       });
     }
   }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+
+      const { id, nome, email } = user;
+      return res.json({ id, nome, email });
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const user = await User.findByPk(req.userId);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      const userAtualizado = await user.update(req.body);
+      return res.json(userAtualizado);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const user = await User.findByPk(req.userId);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      await user.destroy();
+      return res.json(null);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new UserController();
