649fa97faf3300856f377f92fd3826dbb63660fc
jose <joseamando5974@gmail.com>
2026-04-02 18:55:24 -0300

    Adicionando upload de imagens com multer

---
+++ src/routes/fotoRoutes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from '../config/multer';
+import fotoController from '../controllers/FotoController';
+
+const upload = multer(multerConfig);
+
+const router = new Router();
+
+router.post('/', upload.single('foto'), fotoController.store);
+
+export default router;
