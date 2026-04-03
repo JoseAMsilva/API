215c7041986d2354b6448eb06b470607df6f93c9
jose <joseamando5974@gmail.com>
2026-04-03 17:55:25 -0300

    configurando server do swagger

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,5 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT;
 
-_app2.default.listen(PORT);
+_app2.default.listen(port);
