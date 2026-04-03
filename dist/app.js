a6a4f042215723e347304dfbd8a22e80ab057b58
jose <joseamando5974@gmail.com>
2026-04-03 20:42:44 -0300

    Atualizar o middleware de estático para servir imagens a partir do caminho '/images'

--- dist/app.js
+++ dist/app.js
@@ -23,7 +23,7 @@ class App {
   middlewares() {
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
-    this.app.use(_express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
+    this.app.use('/images', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
     this.app.use('/api-docs', _swaggeruiexpress2.default.serve, _swaggeruiexpress2.default.setup(_swagger2.default));
   }
 
